75168e7e65234b93f45fe8a60705b71204c7d84f
WHARY <william.hary@capgemini.com>
2019-01-21 23:18:26 +0100

    Add use of AlertComponent from user list composant for error display

---
+++ src/AlmaIt.dotnet.Heroes.Client/Components/Settings/AddUserBase.cshtml.cs
@@ -0,0 +1,9 @@
+namespace AlmaIt.dotnet.Heroes.Client.Components.Settings
+{
+    using Microsoft.AspNetCore.Blazor.Components;
+    
+    public class AddUserBase : BlazorComponent
+    {
+        
+    }
+}
