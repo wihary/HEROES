41624ad26532b4702c0f3352e096e8b13a816a7a
WHARY <william.hary@capgemini.com>
2019-01-21 23:43:35 +0100

    Added initial logic to add new users with claims

--- src/AlmaIt.dotnet.Heroes.Client/Components/Settings/AddUserBase.cshtml.cs
+++ src/AlmaIt.dotnet.Heroes.Client/Components/Settings/AddUserBase.cshtml.cs
@@ -1,9 +1,49 @@
 namespace AlmaIt.dotnet.Heroes.Client.Components.Settings
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Dotnet.JsonIdentityProvider.IdentityProvider.Model;
+    using Microsoft.AspNetCore.Blazor;
     using Microsoft.AspNetCore.Blazor.Components;
-    
+
     public class AddUserBase : BlazorComponent
     {
-        
+        [Inject]
+        protected HttpClient Http { get; set; }
+
+        protected UserModel user = new UserModel();
+
+        protected string inputClaim = string.Empty;
+
+        protected bool showAddUserPanel = false;
+
+        protected override void OnInit()
+        {
+            this.user.Claims = new List<string>();
+        }
+
+        protected void ToggleShowAddUser()
+        {
+            this.showAddUserPanel = !this.showAddUserPanel;
+            StateHasChanged();
+        }
+
+        protected async Task CreateUser()
+        {
+            await Http.SendJsonAsync(HttpMethod.Post, "/api/user", user);
+        }
+
+        protected void AddClaim()
+        {
+            this.user.Claims.Add($"{this.inputClaim}:True");
+            StateHasChanged();
+        }
+
+        protected void RemoveClaim(string claim)
+        {
+            this.user.Claims.RemoveAll(x => x.Contains(claim));
+        }
     }
 }
