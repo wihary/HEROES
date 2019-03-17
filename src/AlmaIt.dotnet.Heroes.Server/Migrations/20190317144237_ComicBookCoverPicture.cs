f8d03aaee86a02f8b3f5e7ff5161e42322c24500
WHARY <william.hary@capgemini.com>
2019-03-17 15:48:00 +0100

    Add comic book cover picture model properties and db migration

---
+++ src/AlmaIt.dotnet.Heroes.Server/Migrations/20190317144237_ComicBookCoverPicture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlmaIt.Dotnet.Heroes.Server.Migrations
+{
+    public partial class ComicBookCoverPicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "CoverPicture",
+                table: "ComicBooks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CoverUrl",
+                table: "ComicBooks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverPicture",
+                table: "ComicBooks");
+
+            migrationBuilder.DropColumn(
+                name: "CoverUrl",
+                table: "ComicBooks");
+        }
+    }
+}
--- src/AlmaIt.dotnet.Heroes.Shared/Models/ComicBook.cs
+++ src/AlmaIt.dotnet.Heroes.Shared/Models/ComicBook.cs
@@ -9,44 +9,77 @@ namespace AlmaIt.Dotnet.Heroes.Shared.Models
 
     using AlmaIt.Dotnet.Heroes.Shared.Enumeration;
 
-    /// <summary>Represent a comic book or issue.</summary>
+    /// <summary>
+    ///     Represent a comic book or issue.
+    /// </summary>
     public class ComicBook : IDataObject<int>
     {
-        /// <summary>Gets or sets the primary key.</summary>
+        /// <summary>
+        ///     Gets or sets the primary key.
+        /// </summary>
         public int Id { get; set; }
 
-        /// <summary>Gets or sets the primary key of associated comics serie.</summary>
+        /// <summary>
+        ///     Gets or sets the primary key of associated comics serie.
+        /// </summary>
         public int? ComicSerieId { get; set; }
 
-        /// <summary>Gets or sets the instance of the related comic serie.</summary>
+        /// <summary>
+        ///     Gets or sets the instance of the related comic serie.
+        /// </summary>
         public ComicSeries ComicSerie { get; set; }
 
-        /// <summary>Gets or sets the comic book Title.</summary>
+        /// <summary>
+        ///     Gets or sets the comic book Title.
+        /// </summary>
         [Required]
         [DisplayName("Title")]
         public string Title { get; set; } = string.Empty;
 
-        /// <summary>Gets or sets the comic book number.</summary>
+        /// <summary>
+        ///     Gets or sets the comic book number.
+        /// </summary>
         [Required]
         [DisplayName("Issue number")]
         public int IssueNumber { get; set; } = 0;
 
-        /// <summary>Gets or sets the comic book parution date.</summary>
+        /// <summary>
+        ///     Gets or sets the comic book parution date.
+        /// </summary>
         [Required]
         public DateTime ParutionDate { get; set; } = DateTime.Now;
 
-        /// <summary>Gets or sets the object latest update date.</summary>
+        /// <summary>
+        ///     Gets or sets the object latest update date.
+        /// </summary>
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime UpdateDate { get; set; } = DateTime.Now;
 
-        /// <summary>Gets or sets the status of the comic book.</summary>
+        /// <summary>
+        ///     Gets or sets the status of the comic book.
+        /// </summary>
         public ComicBookStatus Status { get; set; } = ComicBookStatus.None;
 
-        /// <summary>Gets or sets list of all associated Tag object. TODO: [JsonIgnore] == we are not yet ready for this, should come with business layer separation.</summary>
+        /// <summary>
+        ///     Gets or sets the comic cover URL
+        /// </summary>
+        public string CoverUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     Gets or sets the comic cover Picture, it's initialize from CoverUrl content recovery
+        /// </summary>
+        public byte[] CoverPicture { get; set; }
+
+        /// <summary>
+        ///     Gets or sets list of all associated Tag object. 
+        ///     TODO: [JsonIgnore] == we are not yet ready for this, should come with business layer separation.
+        /// </summary>
         public ICollection<ComicBookTags> RelatedTags { get; set; }
 
-        /// <summary>Gets the tags list.</summary>
+        /// <summary>
+        ///     Gets the tags list.
+        /// </summary>
         [NotMapped]
         public List<ObjectTag> Tags => this.RelatedTags == null ? new List<ObjectTag>() : this.RelatedTags.Select(t => t.Tag).ToList();
     }
